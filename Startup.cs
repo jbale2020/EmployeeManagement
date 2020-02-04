b7e1b1d60365fc380fd124c6a78c47249526fca8
Jude Marlon Alegro <jude.marlon.b.alegro@accenture.com>
2020-02-04 18:29:25 +0800

    Part 20

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,8 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,27 +32,11 @@ namespace EmployeeManagement
         {
             if (env.IsDevelopment())
             {
-                //DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
-                //{
-                //    SourceCodeLineCount = 1
-                //};
                 app.UseDeveloperExceptionPage();
             }
-            
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response
-            //        .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-            //    });
-            //});
-
+         
             app.UseStaticFiles();
-
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
