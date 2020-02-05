956856e5df1543983c129f841564edfacec22396
Jude Marlon Alegro <jude.marlon.b.alegro@accenture.com>
2020-02-05 15:10:40 +0800

    Part 20+ Details.cshtml from Views/Home folder

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,16 +10,21 @@ namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
 
-        public HomeController()
+        public HomeController(IEmployeeRepository employeeRepository)
         {
-            _employeeRepository = new MockEmployeeRepository();
+            _employeeRepository = employeeRepository;
         }
         public string Index()
         {
             return _employeeRepository.GetEmployee(1).Name;
         }
 
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return View(model); 
+        }
     }
 }
